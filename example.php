a446a3d3e6222529c9d32c3b16419c422fc8c133
Russell Purinton <russell.purinton@gmail.com>
2024-01-20 14:57:04 -0500

    Update example.php

--- example.php
+++ example.php
@@ -5,4 +5,4 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Aifrus\METAR\METAR;
 
 $metar = new METAR('KJFK 111651Z 23012KT 10SM FEW060 SCT250 31/21 A2992 RMK AO2 SLP130 T03060206');
-print_r($response);
+print_r($metar);
