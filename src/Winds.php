9664146df55767b659d23d6e36267a6a5ccdfdcd
Russell Purinton <russell.purinton@gmail.com>
2024-01-20 15:22:06 -0500

    Update Winds.php

--- src/Winds.php
+++ src/Winds.php
@@ -12,29 +12,51 @@ class Winds
     public ?int $gust = null;
     public ?int $variationFrom = null;
     public ?int $variationTo = null;
-    public ?int $variationDirection = null;
 
     public function __construct(string $windString)
     {
         $this->windString = $windString;
+
+        if ($windString === '00000KT') {
+            $this->direction = 0;
+            $this->speed = 0;
+            return;
+        }
+
+        if ($windString === '/////KT') {
+            return;
+        }
+
+        if (substr($windString, 0, 3) === 'VRB') {
+            $this->direction = null;
+            $this->speed = (int) substr($windString, 3, 2);
+            return;
+        }
+
         $this->direction = (int) substr($windString, 0, 3);
         $this->speed = (int) substr($windString, 3, 2);
-        if (strlen($windString) >= 7) $this->gust = (int) substr($windString, 5, 2);
-        if (strlen($windString) >= 10) $this->variationFrom = (int) substr($windString, 7, 3);
-        if (strlen($windString) >= 13) $this->variationTo = (int) substr($windString, 10, 3);
-        if (strlen($windString) >= 16) $this->variationDirection = (int) substr($windString, 13, 3);
+
+        $gustPosition = strpos($windString, 'G');
+        if ($gustPosition !== false) {
+            $this->gust = (int) substr($windString, $gustPosition + 1, 2);
+        }
+
+        $variationPosition = strpos($windString, 'V');
+        if ($variationPosition !== false) {
+            $this->variationFrom = (int) substr($windString, $variationPosition - 3, 3);
+            $this->variationTo = (int) substr($windString, $variationPosition + 1, 3);
+        }
+
         $this->validate();
     }
 
     public function validate(): void
     {
-        if ($this->direction < 0 || $this->direction > 360) throw new METARException('Invalid wind direction');
-        if ($this->speed < 0 || $this->speed > 99) throw new METARException('Invalid wind speed');
-
-        if ($this->gust !== null && ($this->gust < 0 || $this->gust > 99)) throw new METARException('Invalid wind gust');
+        if ($this->direction !== null && ($this->direction < 0 || $this->direction > 360)) throw new METARException('Invalid wind direction');
+        if ($this->speed !== null && $this->speed < 0) throw new METARException('Invalid wind speed');
+        if ($this->gust !== null && ($this->gust < 0 || ($this->gust - $this->speed) < 10)) throw new METARException('Invalid wind gust');
         if ($this->variationFrom !== null && ($this->variationFrom < 0 || $this->variationFrom > 360)) throw new METARException('Invalid wind variation from');
         if ($this->variationTo !== null && ($this->variationTo < 0 || $this->variationTo > 360)) throw new METARException('Invalid wind variation to');
-        if ($this->variationDirection !== null && ($this->variationDirection < 0 || $this->variationDirection > 360)) throw new METARException('Invalid wind variation direction');
     }
 
     public static function create(string $windString): Winds
