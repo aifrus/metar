b03746f7a2465b4fcbe9d46e46934896b7809e99
Russell Purinton <russell.purinton@gmail.com>
2024-01-20 15:23:05 -0500

    Update example.php

--- example.php
+++ example.php
@@ -4,5 +4,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Aifrus\METAR\METAR;
 
-$metar = METAR::fetch('KJFK');
+if (!isset($argv[1])) die("Usage: php example.php <ICAO>\n");
+$metar = METAR::fetch($argv[1]);
 print_r($metar);
