a78a517bed84378056f5a9f3240c86d099e79de4
Russell Purinton <russell.purinton@gmail.com>
2024-01-20 06:09:58 -0500

    x

--- example.php
+++ example.php
@@ -5,4 +5,4 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Aifrus\METAR\METAR;
 
 $response = METAR::fetch("KBGR,KPWM,KSFM,KMHT,KPSM,KBOS");
-echo($response . PHP_EOL);
+echo ($response . PHP_EOL);
