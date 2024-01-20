028badbb41ecb40a93140628e22ee674cb8215fb
Russell Purinton <russell.purinton@gmail.com>
2024-01-20 14:35:01 -0500

    Update example.php

--- example.php
+++ example.php
@@ -6,5 +6,7 @@ use Aifrus\METAR\METAR;
 
 $response = METAR::fetch("KPWM");
 print_r($response);
-$response = METAR::fetch("LPLA");
-print_r($response);
+
+$metar = new METAR("SPECI KPWM 221853Z 00000KT 10SM CLR 22/12 A3010 RMK AO2 SLP191 T02220117");
+print_r($metar);
+
