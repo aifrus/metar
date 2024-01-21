be8c536e40b78d4d1156a034b2f19dacb9068335
Russell Purinton <russell.purinton@gmail.com>
2024-01-21 09:48:29 -0500

    Update test.php

--- test.php
+++ test.php
@@ -7,11 +7,11 @@ use RPurinton\HTTPS\HTTPSRequest;
 
 $all = explode("\n", HTTPSRequest::fetch(['url' => 'https://metar.vatsim.net/ALL']));
 foreach ($all as $key => $line) {
-    echo "\r$key/", count($all);
+    echo "\r$key/", count($all) . " $line";
     try {
         new METAR($line);
     } catch (\Exception $e) {
-        echo "Exception\n" . $e->getMessage() . "\nOn line: $line\n";
+        echo "\nException: " . $e->getMessage() . "\nOn line: $line\n";
     }
 }
 echo "done!\n";
