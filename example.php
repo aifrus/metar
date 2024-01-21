29793c6864611480dcee0f4c9633489b8989fa84
Russell Purinton <russell.purinton@gmail.com>
2024-01-21 09:39:52 -0500

    Update example.php

--- example.php
+++ example.php
@@ -3,7 +3,22 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Aifrus\METAR\METAR;
+use RPurinton\HTTPS\HTTPSRequest;
 
-if (!isset($argv[1])) die("Usage: php example.php <ICAO>\n");
-$metar = METAR::fetch($argv[1]);
-print_r($metar);
+$all = HTTPSRequest::fetch(['url' => 'https://metar.vatsim.net/ALL']);
+$all = explode("\n", $all);
+$count = count($all);
+$counter = 0;
+$metars = [];
+foreach ($all as $line) {
+    $counter++;
+    if ($counter % 100 === 0) echo "\r$counter/$count...";
+    $metar = trim($metar);
+    if (empty($metar)) continue;
+    try {
+        $metars[] = new METAR($metar);
+    } catch (\Exception $e) {
+        echo "Exception\n" . $e->getMessage() . "\nOn line: $metar\n";
+    }
+}
+echo "done!\n";
