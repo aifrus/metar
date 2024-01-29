fa2aba3c5e5ca8250a42ba2f53284341bb0cd59a
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2024-01-29 02:08:10 -0500

    Update METAR.php

--- src/METAR.php
+++ src/METAR.php
@@ -8,7 +8,6 @@ use Aifrus\METAR\Parts\StationIdentifier;
 use Aifrus\METAR\Parts\Timestamp;
 use Aifrus\METAR\Parts\ReportModifier;
 use Aifrus\METAR\Parts\Winds;
-use Aifrus\METAR\Exceptions\METARException;
 
 class METAR
 {
@@ -16,30 +15,38 @@ class METAR
 	public ?ReportType $reportType = null;
 	public ?StationIdentifier $stationIdentifier = null;
 	public ?Timestamp $timeStamp = null;
-	public ?ReportModifier $reportModifier = null;
+	public array $reportModifiers = [];
 	public ?Winds $winds = null;
 	public ?bool $requiresMaintenance = null;
 
-	public static function create(string $reportString): METAR
+	public static function create(string $reportString): self
 	{
-		return new METAR($reportString);
+		return (new self)->fill($reportString);
 	}
 
-	public function __construct(string $reportString)
+	public function fill(string $reportString): self
 	{
 		$this->reportString = trim($reportString);
 		$reportParts = explode(' ', $this->reportString);
+		$this->extractReportModifiers($reportParts);
 		$this->reportType = ReportType::create($reportParts[0]);
 		if (in_array($reportParts[0], ['METAR', 'SPECI'])) array_shift($reportParts);
-		if (ReportModifierEnum::isA($reportParts[0])) $this->reportModifier = ReportModifier::create(array_shift($reportParts));
 		$this->stationIdentifier = StationIdentifier::create(array_shift($reportParts));
-		if (ReportModifierEnum::isA($reportParts[0])) $this->reportModifier = ReportModifier::create(array_shift($reportParts));
 		$this->timeStamp = Timestamp::create(array_shift($reportParts));
-		if (ReportModifierEnum::isA($reportParts[0])) $this->reportModifier = ReportModifier::create(array_shift($reportParts));
-		if (!$this->reportModifier) $this->reportModifier = ReportModifier::create('NONE');
 		$this->winds = Winds::create(array_shift($reportParts), $reportParts[0] ?? null);
 		if ($this->winds->variableRange) array_shift($reportParts);
 		$this->requiresMaintenance = substr($this->reportString, -1) === '$';
+		return $this;
+	}
+
+	private function extractReportModifiers(array &$reportParts): void
+	{
+		foreach ($reportParts as $key => $reportPart) if (ReportModifierEnum::isA($reportPart)) {
+			$this->reportModifiers[] = ReportModifier::create($reportPart);
+			unset($reportParts[$key]);
+		}
+		if (empty($this->reportModifiers)) $this->reportModifiers[] = ReportModifier::create('NONE');
+		$reportParts = array_values($reportParts);
 	}
 
 	public function __toString(): string
